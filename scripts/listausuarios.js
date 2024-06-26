b6fea3ce46ea5f4d1e1f5341020909bf0f168f75
RAULPC\rgr_1 <rgr182>
2024-06-26 15:50:43 -0600

    Added Users API CALL

---
+++ scripts/listausuarios.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.ajax({
+        url: 'https://localhost:7100/Users',
+        type: 'GET',
+        success: function(user) {
+            $("#usuarios-lista").append("<li>" + user + "</li>");            
+        },
+        error: function(error) {
+            console.log("Error:", error);
+        }
+    });
+});
